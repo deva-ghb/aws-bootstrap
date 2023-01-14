9efac49054398f88ccbf514fc886f5a645b9735c
deva-git-hub <deva.mailaddress.conf@gmail.com>
2023-01-14 22:08:51 +0530

    Add codebuild / codedeploy spec

---
+++ deploy-infra.sh
@@ -0,0 +1,58 @@
+# set STACK_NAME=awsbootstrap 
+# set REGION=ap-south-1
+# set CLI_PROFILE=awsbootstrap
+
+# set EC2_INSTANCE_TYPE=t2.micro 
+
+# FOR /F "tokens=*" %%g IN ('aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text') do (SET AWS_ACCOUNT_ID=%%g)
+
+# set CODEPIPELINE_BUCKET="%STACK_NAME%-%REGION%-codepipeline-%AWS_ACCOUNT_ID%"
+
+# echo -e "\n\n=========== Deploying main.yml ==========="
+# :: aws cloudformation deploy --region %REGION% --profile %CLI_PROFILE% --stack-name %STACK_NAME% --template-file main.yml --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM --parameter-overrides  EC2InstanceType=%EC2_INSTANCE_TYPE%
+# echo -e "\n\n=========== Deploying setup.yml ==========="
+# aws cloudformation deploy --region %REGION% --profile %CLI_PROFILE% --stack-name %STACK_NAME%-setup --template-file setup.yml --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM --parameter-overrides CodePipelineBucket=%CODEPIPELINE_BUCKET%
+
+# script
+
+STACK_NAME=awsbootstrap 
+REGION=ap-south-1
+CLI_PROFILE=awsbootstrap
+EC2_INSTANCE_TYPE=t2.micro 
+
+# FOR /F "tokens=*" %%g IN ('aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text') do (SET AWS_ACCOUNT_ID=%%g)
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
+
+# aws cloudformation deploy --region %REGION% --profile %CLI_PROFILE% --stack-name %STACK_NAME% --template-file main.yml --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM --parameter-overrides  EC2InstanceType=%EC2_INSTANCE_TYPE%
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+
+    # If the deploy succeeded, show the DNS name of the created instance
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile awsbootstrap \
+    --query "Exports[?Name=='InstanceEndpoint'].Value" 
+fi
